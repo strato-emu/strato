875fe194f61a09e7ffb6f3cb87956b4270a32b26
◱ PixelyIon <dhruvmarkcollins@gmail.com>
2019-06-29 19:05:24 +0530

    Structure Code and Link libunicorn

--- app/src/main/java/gq/cyuubi/lightswitch/MainActivity.java
+++ app/src/main/java/gq/cyuubi/lightswitch/MainActivity.java
@@ -1,15 +1,14 @@
 package gq.cyuubi.lightswitch;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 import android.widget.TextView;
 
+import androidx.appcompat.app.AppCompatActivity;
+
 public class MainActivity extends AppCompatActivity {
 
-    // Used to load the 'native-lib' library on application startup.
     static {
-        System.loadLibrary("native-lib");
+        System.loadLibrary("lightswitch");
     }
 
     @Override
@@ -22,9 +21,5 @@ public class MainActivity extends AppCompatActivity {
         tv.setText(stringFromJNI());
     }
 
-    /**
-     * A native method that is implemented by the 'native-lib' native library,
-     * which is packaged with this application.
-     */
     public native String stringFromJNI();
 }
