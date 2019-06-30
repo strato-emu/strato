64f2f9008772a3fdbc7011a359470a98e4d75f09
Ryan Teal <rynteal93@gmail.com>
2019-06-30 15:35:25 +0100

    Add external storage permission dialog

--- app/src/main/java/gq/cyuubi/lightswitch/MainActivity.java
+++ app/src/main/java/gq/cyuubi/lightswitch/MainActivity.java
@@ -1,9 +1,13 @@
 package gq.cyuubi.lightswitch;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -15,6 +19,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        if(ContextCompat.checkSelfPermission(MainActivity.this, Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED)
+        {
+            ActivityCompat.requestPermissions(MainActivity.this,
+                    new String[]{Manifest.permission.READ_EXTERNAL_STORAGE},
+                    1);
+        }
 
         // Example of a call to a native method
         TextView tv = findViewById(R.id.sample_text);
