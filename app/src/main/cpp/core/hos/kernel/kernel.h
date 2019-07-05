e44f7f8feb7f110910f1b467e2f8fdaa7166ebfe
Ryan Teal <rynteal93@gmail.com>
2019-07-05 13:33:58 +0100

    Basic KObject implementation

---
+++ app/src/main/cpp/core/hos/kernel/kernel.h
@@ -0,0 +1,19 @@
+#pragma once
+#include <cstdint>
+#include <memory>
+
+namespace core::kernel
+{
+    class KObject
+    {
+    public:
+        KObject(uint32_t handle) : handle(handle) {}
+        uint32_t Handle() { return handle; }
+    private:
+        uint32_t handle;
+    };
+
+    typedef std::shared_ptr<KObject> KObjectPtr;
+
+    uint32_t NewHandle(KObjectPtr obj);
+}
