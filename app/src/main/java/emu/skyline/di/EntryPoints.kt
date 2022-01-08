4be8b4cf66ba2036169028f64d15e4e7cebe06fe
lynxnb <niccolo.betto@gmail.com>
2022-01-08 18:48:02 +0100

    Add missing SPDX licence header

--- app/src/main/java/emu/skyline/di/EntryPoints.kt
+++ app/src/main/java/emu/skyline/di/EntryPoints.kt
@@ -1,3 +1,8 @@
+/*
+ * SPDX-License-Identifier: MPL-2.0
+ * Copyright © 2020 Skyline Team and Contributors (https://github.com/skyline-emu/)
+ */
+
 package emu.skyline.di
 
 import android.content.Context
