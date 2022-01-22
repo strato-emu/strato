6f17d1351f8d3861bf003c23b4d3737609907d26
Billy Laws <blaws05@gmail.com>
2022-01-22 21:47:18 +0000

    Fixup ordering for B10G11R11Float texture format

--- app/src/main/cpp/skyline/gpu/texture/format.h
+++ app/src/main/cpp/skyline/gpu/texture/format.h
@@ -71,12 +71,12 @@ namespace skyline::gpu::format {
     FORMAT(B5G5R5A1Unorm, 16, eB5G5R5A1UnormPack16);
     FORMAT_INT_FLOAT(R32, 32, eR32);
     FORMAT_NORM_INT_FLOAT(R16G16, 32, eR16G16);
-    FORMAT(B10G11R11Float, 32, eB10G11R11UfloatPack32);
     FORMAT(R11G11B10Float, 32, eB10G11R11UfloatPack32, .swizzle = {
         .red = swc::Blue,
         .green = swc::Green,
         .blue = swc::Red,
    });
+    FORMAT(B10G11R11Float, 32, eB10G11R11UfloatPack32);
     FORMAT_NORM_INT_SRGB(R8G8B8A8, 32, eR8G8B8A8);
     FORMAT_NORM_INT_SRGB(G8B8A8R8, 32, eB8G8R8A8, .swizzle = {
         .blue = swc::Alpha,
