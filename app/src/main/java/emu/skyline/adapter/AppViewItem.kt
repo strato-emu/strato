TheViComte/skyline

`AppListItem` misc tweaks
* Restore text marquee on all layouts
* Text size and color tweaks
* List layout image has round corners
* Clean up unneeded attributes

app/src/main/java/emu/skyline/adapter/AppViewItem.kt
@@ -87,6 +87,9 @@ class AppViewItem(var layoutType : LayoutType, private val item : AppItem, priva
     override fun bind(binding : LayoutBinding<*>, position : Int) {
         binding.textTitle.text = item.title
         binding.textSubtitle.text = item.subTitle ?: item.loaderResultString(binding.root.context)
+        // Make the title text view selected for marquee to work
+        binding.textTitle.isSelected = true
+        binding.textSubtitle.isSelected = true
 
         binding.icon.setImageBitmap(item.icon ?: missingIcon)
 

strato-emu/strato

app/src/main/java/emu/skyline/adapter/AppViewItem.kt
@@ -97,6 +97,10 @@ class AppViewItem(var layoutType : LayoutType, private val item : AppItem, priva
         binding.textTitle.text = item.title
         binding.textVersion.text = item.version ?: item.loaderResultString(binding.root.context)
         binding.textAuthor.text = item.author
+        // Make text views selected for marquee to work
+        binding.textTitle.isSelected = true
+        binding.textVersion.isSelected = true
+        binding.textAuthor.isSelected = true
 
         binding.icon.setImageBitmap(item.icon ?: missingIcon)
 