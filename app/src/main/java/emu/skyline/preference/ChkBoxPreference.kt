7cd13916a3ffffeb70e19bf328d39db7228505a2
hacobot.dev <hacobot.dev@gmail.com>
2023-01-10 17:23:49 -0700

    Main activity is now refreshing when the group checkbox is changed

---
+++ app/src/main/java/emu/skyline/preference/ChkBoxPreference.kt
@@ -0,0 +1,23 @@
+/*
+ * SPDX-License-Identifier: MPL-2.0
+ * Copyright © 2020 Skyline Team and Contributors (https://github.com/skyline-emu/)
+ */
+
+package emu.skyline.preference
+
+import android.content.Context
+import android.util.AttributeSet
+import androidx.preference.CheckBoxPreference
+import androidx.preference.R
+import emu.skyline.di.getSettings
+
+/**
+ * This preference is used with checkboxes that need to refresh the main activity when changed
+ */
+class ChkBoxPreference @JvmOverloads constructor(context : Context, attrs : AttributeSet? = null, defStyleAttr : Int = R.attr.checkBoxPreferenceStyle) : CheckBoxPreference(context, attrs, defStyleAttr) {
+
+    override fun onClick() {
+        context?.getSettings()?.refreshRequired = true
+        super.onClick()
+    }
+}
