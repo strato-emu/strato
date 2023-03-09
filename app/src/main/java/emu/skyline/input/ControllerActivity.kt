ab553c9671053457c98a7263682e0100a1600d62
lynxnb <niccolo.betto@gmail.com>
2023-03-09 23:26:20 +0100

    Use MD3 divider in `ControllerActivity`

--- app/src/main/java/emu/skyline/input/ControllerActivity.kt
+++ app/src/main/java/emu/skyline/input/ControllerActivity.kt
@@ -234,8 +234,6 @@ class ControllerActivity : AppCompatActivity() {
                 }
             }
         }
-
-        dividerItemDecoration.drawable.let { it?.setTint(obtainStyledAttributes(intArrayOf(R.attr.colorOutlineVariant)).use { attr -> attr.getColor(0, Color.BLACK) }); it }
         binding.controllerList.addItemDecoration(dividerItemDecoration)
 
         update()
