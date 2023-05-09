d2436423b0b045dfc6f56c9d1dc4c4230fa06b2b
QuackingCanary <adrien.bouillon7@etu.univ-lorraine.fr>
2023-05-10 00:13:32 +0200

    Added toggle to disable the pause button in EmulationActivity

--- app/src/main/java/emu/skyline/EmulationActivity.kt
+++ app/src/main/java/emu/skyline/EmulationActivity.kt
@@ -347,6 +347,11 @@ class EmulationActivity : AppCompatActivity(), SurfaceHolder.Callback, View.OnTo
 
         binding.onScreenPauseToggle.apply {
             isGone = binding.onScreenControllerView.isGone
+            if(!emulationSettings.showPauseButton) {
+                binding.onScreenPauseToggle.visibility = View.GONE
+            } else {
+                binding.onScreenPauseToggle.visibility = View.VISIBLE
+            }
             setOnClickListener {
                 if (isEmulatorPaused) {
                     resumeEmulator()
@@ -476,7 +481,7 @@ class EmulationActivity : AppCompatActivity(), SurfaceHolder.Callback, View.OnTo
                 isGone = binding.onScreenControllerView.isGone
             }
             binding.onScreenPauseToggle.apply {
-                isGone = binding.onScreenControllerView.isGone
+                isGone = !emulationSettings.showPauseButton
             }
         }
     }
