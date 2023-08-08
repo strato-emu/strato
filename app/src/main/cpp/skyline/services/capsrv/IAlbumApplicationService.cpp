c43912c1f8c126ec045206023041bf89623926b7
PabloG02 <tioo23000@gmail.com>
2023-08-08 20:45:07 +0200

    Stub GetAlbumFileList0AafeAruidDeprecated

--- app/src/main/cpp/skyline/services/capsrv/IAlbumApplicationService.cpp
+++ app/src/main/cpp/skyline/services/capsrv/IAlbumApplicationService.cpp
@@ -6,6 +6,19 @@
 namespace skyline::service::capsrv {
     IAlbumApplicationService::IAlbumApplicationService(const DeviceState &state, ServiceManager &manager) : BaseService(state, manager) {}
 
+    Result IAlbumApplicationService::GetAlbumFileList0AafeAruidDeprecated(type::KSession &session, ipc::IpcRequest &request, ipc::IpcResponse &response) {
+        u64 totalOutputEntries{0};
+
+        auto pid{request.Pop<i32>()};
+        auto contentType{request.Pop<ContentType>()};
+        auto albumFileDateTimeStart{request.Pop<u64>()};
+        auto albumFileDateTimeEnd{request.Pop<u64>()};
+        auto appletResourceUserId{request.Pop<u64>()};
+
+        response.Push<u64>(totalOutputEntries);
+        return {};
+    }
+
     Result IAlbumApplicationService::GetAlbumFileList3AaeAruid(type::KSession &session, ipc::IpcRequest &request, ipc::IpcResponse &response) {
         u64 totalOutputEntries{0};
 
