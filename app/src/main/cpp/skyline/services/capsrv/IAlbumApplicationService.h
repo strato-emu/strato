5604dc8be55576924273ff05dce2fcc22dbb663f
PabloG02 <tioo23000@gmail.com>
2023-08-08 20:45:07 +0200

    Stub GetAlbumFileList0AafeAruidDeprecated

--- app/src/main/cpp/skyline/services/capsrv/IAlbumApplicationService.h
+++ app/src/main/cpp/skyline/services/capsrv/IAlbumApplicationService.h
@@ -24,11 +24,17 @@ namespace skyline::service::capsrv {
         IAlbumApplicationService(const DeviceState &state, ServiceManager &manager);
 
         /**
+         * @url https://switchbrew.org/wiki/Capture_services#GetAlbumFileList0AafeAruidDeprecated
+         */
+        Result GetAlbumFileList0AafeAruidDeprecated(type::KSession &session, ipc::IpcRequest &request, ipc::IpcResponse &response);
+
+        /**
          * @url https://switchbrew.org/wiki/Capture_services#GetAlbumFileList3AaeAruid
          */
         Result GetAlbumFileList3AaeAruid(type::KSession &session, ipc::IpcRequest &request, ipc::IpcResponse &response);
 
         SERVICE_DECL(
+            SFUNC(0x66, IAlbumApplicationService, GetAlbumFileList0AafeAruidDeprecated),
             SFUNC(0x8E, IAlbumApplicationService, GetAlbumFileList3AaeAruid)
         )
     };
