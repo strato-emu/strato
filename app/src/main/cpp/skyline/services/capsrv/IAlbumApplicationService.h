cdeb2d37571b7f049d60b60177711a4b16e46b63
PabloG02 <tioo23000@gmail.com>
2023-08-08 19:44:41 +0200

    Stub GetAlbumFileList3AaeAruid

--- app/src/main/cpp/skyline/services/capsrv/IAlbumApplicationService.h
+++ app/src/main/cpp/skyline/services/capsrv/IAlbumApplicationService.h
@@ -10,7 +10,26 @@ namespace skyline::service::capsrv {
      * @url https://switchbrew.org/wiki/Capture_services#caps:u
      */
     class IAlbumApplicationService : public BaseService {
+      private:
+        /**
+         * @url https://switchbrew.org/wiki/Capture_services#ContentType
+         */
+        enum class ContentType : u8 {
+            Screenshot = 0,
+            Movie = 1,
+            ExtraMovie = 3,
+        };
+
       public:
         IAlbumApplicationService(const DeviceState &state, ServiceManager &manager);
+
+        /**
+         * @url https://switchbrew.org/wiki/Capture_services#GetAlbumFileList3AaeAruid
+         */
+        Result GetAlbumFileList3AaeAruid(type::KSession &session, ipc::IpcRequest &request, ipc::IpcResponse &response);
+
+        SERVICE_DECL(
+            SFUNC(0x8E, IAlbumApplicationService, GetAlbumFileList3AaeAruid)
+        )
     };
 }
