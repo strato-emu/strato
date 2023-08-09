a523af21297cbd3e3ad9f56ca910bbc459d21162
PabloG02 <tioo23000@gmail.com>
2023-08-09 23:23:14 +0200

    Stub some services related to StereoVision
    Based mostly in Ryujinx

--- app/src/main/cpp/skyline/services/pctl/IParentalControlService.h
+++ app/src/main/cpp/skyline/services/pctl/IParentalControlService.h
@@ -6,11 +6,23 @@
 #include <services/serviceman.h>
 
 namespace skyline::service::pctl {
+    namespace result {
+        constexpr Result StereoVisionDenied(142, 104);
+        constexpr Result PermissionDenied(142, 133);
+        constexpr Result StereoVisionRestrictionConfigurableDisabled(142, 181);
+    }
+
     /**
      * @brief IParentalControlService is used to access parental control configuration
      * @url https://switchbrew.org/wiki/Parental_Control_services#IParentalControlService
      */
     class IParentalControlService : public BaseService {
+      private:
+        bool featuresRestriction = false;
+        bool stereoVisionRestrictionConfigurable = true;
+        bool stereoVisionRestriction = false;
+
+        Result IsStereoVisionPermittedImpl();
       public:
         IParentalControlService(const DeviceState &state, ServiceManager &manager);
 
@@ -21,15 +33,33 @@ namespace skyline::service::pctl {
 
         Result CheckFreeCommunicationPermission(type::KSession &session, ipc::IpcRequest &request, ipc::IpcResponse &response);
 
+        Result ConfirmStereoVisionPermission(type::KSession &session, ipc::IpcRequest &request, ipc::IpcResponse &response);
+
         Result EndFreeCommunication(type::KSession &session, ipc::IpcRequest &request, ipc::IpcResponse &response);
 
         Result IsFreeCommunicationAvailable(type::KSession &session, ipc::IpcRequest &request, ipc::IpcResponse &response);
 
-        SERVICE_DECL(
+        Result ConfirmStereoVisionRestrictionConfigurable(type::KSession &session, ipc::IpcRequest &request, ipc::IpcResponse &response);
+
+        Result GetStereoVisionRestriction(type::KSession &session, ipc::IpcRequest &request, ipc::IpcResponse &response);
+
+        Result SetStereoVisionRestriction(type::KSession &session, ipc::IpcRequest &request, ipc::IpcResponse &response);
+
+        Result ResetConfirmedStereoVisionPermission(type::KSession &session, ipc::IpcRequest &request, ipc::IpcResponse &response);
+
+        Result IsStereoVisionPermitted(type::KSession &session, ipc::IpcRequest &request, ipc::IpcResponse &response);
+
+      SERVICE_DECL(
             SFUNC(0x1, IParentalControlService, Initialize),
             SFUNC(0x3E9, IParentalControlService, CheckFreeCommunicationPermission),
+            SFUNC(0x3F5, IParentalControlService, ConfirmStereoVisionPermission),
             SFUNC(0x3F9, IParentalControlService, EndFreeCommunication),
-            SFUNC(0x3FA, IParentalControlService, IsFreeCommunicationAvailable)
+            SFUNC(0x3FA, IParentalControlService, IsFreeCommunicationAvailable),
+            SFUNC(0x425, IParentalControlService, ConfirmStereoVisionRestrictionConfigurable),
+            SFUNC(0x426, IParentalControlService, GetStereoVisionRestriction),
+            SFUNC(0x427, IParentalControlService, SetStereoVisionRestriction),
+            SFUNC(0x428, IParentalControlService, ResetConfirmedStereoVisionPermission),
+            SFUNC(0x429, IParentalControlService, IsStereoVisionPermitted)
         )
     };
 }
